c905bb91e0948fd64e7258485c43c0ab2a5ee88b
alexbananabob <alexbananabob@gmai.com>
2015-12-03 18:02:17 +0300

    was done todo-widget by design

--- src/js/view.js
+++ src/js/view.js
@@ -1,52 +1,54 @@
 
-(function (window){
+{
     'use strict'
-    function View () {
-        this.$todoList = document.getElementById('todo-list');
-        this.deafultTemplate
-            =       '<label for="{{id}}" class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect is-checked">'
-            +           '<input type="checkbox" id="{{id}}" {{checked}} class="mdl-checkbox__input" />'
-            +           '<span class="mdl-checkbox__label">{{title}}</span>'
-            +       '</label>'
-            +       '<button for = "{{id}}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored destroy">'
-            +           '<i class="material-icons">clear</i>'
-            +       '</button>'
+    class View {
+        constructor() {
+            this.$todoList = document.getElementById('todo-list');
+            this.deafultTemplate
+                = '<label for="{{id}}" class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect is-checked">'
+                + '<input type="checkbox" id="{{id}}" {{checked}} class="mdl-checkbox__input" />'
+                + '<span class="mdl-checkbox__label">{{title}}</span>'
+                + '</label>'
+                + '<button for = "{{id}}" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored destroy">'
+                + '<i class="material-icons">clear</i>'
+                + '</button>'
 
-    };
+        }
 
 //view data from Model
-    View.prototype._prepareData = function (data) {
-        var template = this.deafultTemplate;
-        template = template.replace('{{id}}', data.id);
-        template = template.replace('{{id}}', data.id);
-        template = template.replace('{{id}}', data.id);
-        template = template.replace('{{title}}', data.title);
-        if (data.completed === true){
-            template = template.replace('{{checked}}', 'checked');
+        _prepareData(data) {
+            var template = this.deafultTemplate;
+            template = template.replace('{{id}}', data.id);
+            template = template.replace('{{id}}', data.id);
+            template = template.replace('{{id}}', data.id);
+            template = template.replace('{{title}}', data.title);
+            if (data.completed === true) {
+                template = template.replace('{{checked}}', 'checked');
+            }
+            return template;
+            //template = template.replace('{{completed}}', completed);
+            //template = template.replace('{{checked}}', checked);
         }
-        return template;
-        //template = template.replace('{{completed}}', completed);
-        //template = template.replace('{{checked}}', checked);
-    };
 
-    View.prototype.showAll = function (database) {
-        this.$todoList.innerHTML = '';
-        for (var i = 0; i < database.length; i++) {
+        showAll(database) {
+            this.$todoList.innerHTML = '';
+            for (var i = 0; i < database.length; i++) {
+                var newLi = document.createElement('li');
+                newLi.innerHTML = this._prepareData(database[i])
+                this.$todoList.appendChild(newLi);
+            }
+            ;
+            componentHandler.upgradeAllRegistered();
+        }
+
+        show(data) {
             var newLi = document.createElement('li');
-            newLi.innerHTML = this._prepareData(database[i])
+            newLi.innerHTML = this._prepareData(data)
             this.$todoList.appendChild(newLi);
-        };
-        componentHandler.upgradeAllRegistered();
-    };
-
-    View.prototype.show = function (data) {
-        var newLi = document.createElement('li');
-        newLi.innerHTML = this._prepareData(data)
-        this.$todoList.appendChild(newLi);
-        componentHandler.upgradeAllRegistered();
-    };
-
+            componentHandler.upgradeAllRegistered();
+        }
+    }
 //export to window
     window.todoApp = window.todoApp || {};
     window.todoApp.View = View;
-}(window));
+}
--- src/js/zmain.js
+++ src/js/zmain.js
@@ -1,13 +1,14 @@
-(function () {
+{
     'use strict';
 
-    function Todo() {
-        this.model = new todoApp.Model();
-        this.view = new todoApp.View();
-        this.controller = new todoApp.Controller(this.model, this.view);
-    }
+    class Todo {
+        constructor () {
+            this.model = new todoApp.Model();
+            this.view = new todoApp.View();
+            this.controller = new todoApp.Controller(this.model, this.view);
+    }}
 
     var todo = new Todo();
 
-})();
+}
 
