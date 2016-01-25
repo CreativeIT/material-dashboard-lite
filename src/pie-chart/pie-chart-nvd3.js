ebf3d06568b14563c453b2ed13f60d999f17cc75
alexbananabob <alexbananabob@gmai.com>
2016-01-25 19:07:52 +0300

    after review pie-chart

--- src/pie-chart/pie-chart-nvd3.js
+++ src/pie-chart/pie-chart-nvd3.js
@@ -1,141 +1,140 @@
-var colors = [
-    "rgba(96, 196, 150, 1)",
-    "rgba(80, 150, 215, 1)",
-    "rgba(0, 188, 212, 1)",
-    "rgba(116, 199, 209, 1)",
-    "rgba(255, 82, 82, 1)",
-    "rgba(0, 0, 0, 0)"
+{
+    var colors = [
+        "rgba(96, 196, 150, 1)",
+        "rgba(80, 150, 215, 1)",
+        "rgba(0, 188, 212, 1)",
+        "rgba(116, 199, 209, 1)",
+        "rgba(255, 82, 82, 1)",
+        "rgba(0, 0, 0, 0)"
     ];
 
-
-var data = [
-    {
-        "key": "Coding",
-        "y" : 0,
-        "end" : 9
-    } ,
-    {
-        "key": "Eating",
-        "y" : 0,
-        "end" : 3
-    } ,
-    {
-        "key": "Sleeping",
-        "y" : 0,
-        "end" : 3
-    } ,
-    {
-        "key": "Meditation",
-        "y" : 0,
-        "end" : 3
-    } ,
-    {
-        "key": "The fight against evil",
-        "y" : 0,
-        "end" : 6
-    }
-    ,
-    {   "key": "Pending",
-        "y": 23.9
-    }
-];
-
-nv.addGraph(function() {
-    var innerRadius = 0.86,
-        outerRadius = 1.02;
-
-    var chart1 = nv.models.pieChart()
-        .x(function(d) { return d.key })
-        .y(function(d) { return d.y })
-        .showLabels(false)
-        .donut(true)
-        .donutRatio(0.65)        // hole size
-        .growOnHover(true)
-        .padAngle(.04)
-        .cornerRadius(0)
-        .margin({"left":-10,"right":-10,"top":-10,"bottom":-10})
-        .color(colors)
-        .id('donut1'); // allow custom CSS for this one svg
-    chart1.arcsRadius([ {"inner":innerRadius,"outer":outerRadius},
-                        {"inner":innerRadius,"outer":outerRadius},
-                        {"inner":innerRadius,"outer":outerRadius},
-                        {"inner":innerRadius,"outer":outerRadius},
-                        {"inner":innerRadius,"outer":outerRadius}
-                      ]);
-    chart1.tooltip.enabled(true)
-                  .hideDelay(0)
-                  .headerEnabled(false)
-                  .contentGenerator(function(d) {
-                        if (d === null) {
-                            return '';
-                        }
-                        d3.select(".nvtooltip").classed("mdl-tooltip",true);
-                        return d.data.y + ' hours';
-                  });
-
-    chart1.showLegend(false);
-
-    chart1.title("0 hours");
-    chart1.titleOffset(10);
-
-    d3.select("#chart")
-        .append("div")
-        .attr("class", "wrap")
-        .append("svg")
-        .datum(data)
-        .transition().duration(1200)
-        .call(chart1);
-
-    var h = 0, i = 0;
-    var timer = setInterval(animate, 70, data);
-
-    function animate (data) {
-        if (i >= data.length - 1) {
-            data.splice(data.length - 1, 1);
-            clearInterval(timer);
-            return;
-        } else {
-            if (data[i].y < data[i].end) {
-                data[i].y++;
-                data[data.length - 1].y--;
-                chart1.title((h + 1) + " hours");
-                h++;
-            }
-            else {
-                i++;
-            }
+    var data = [
+        {
+            "key": "Coding",
+            "y": 0,
+            "end": 9
+        },
+        {
+            "key": "Eating",
+            "y": 0,
+            "end": 3
+        },
+        {
+            "key": "Sleeping",
+            "y": 0,
+            "end": 3
+        },
+        {
+            "key": "Meditation",
+            "y": 0,
+            "end": 3
+        },
+        {
+            "key": "The fight against evil",
+            "y": 0,
+            "end": 6
         }
-        chart1.update();
-    }
+        ,
+        {
+            "key": "Pending",
+            "y": 23.9
+        }
+    ];
 
-    d3.select(".nv-pie .nv-pie")
-        .append("image")
-        .attr("width", "30")
-        .attr("height", "30")
-        .attr("xlink:href", "images/watch_white.svg")
-        .attr("transform", "translate(-15,-35)");
+    nv.addGraph(() => {
+        var innerRadius = 0.86,
+            outerRadius = 1.02;
 
-    var color = d3.scale.ordinal().range(colors);
+        var pieChart = nv.models.pieChart()
+            .x(d => {return d.key})
+            .y(d => {return d.y})
+            .showLabels(false)
+            .donut(true)
+            .growOnHover(true)
+            .padAngle(.04)
+            .cornerRadius(0)
+            .margin({"left": -10, "right": -10, "top": -10, "bottom": -10})
+            .color(colors)
+            .arcsRadius([{"inner": innerRadius, "outer": outerRadius},
+                         {"inner": innerRadius, "outer": outerRadius},
+                         {"inner": innerRadius, "outer": outerRadius},
+                         {"inner": innerRadius, "outer": outerRadius},
+                         {"inner": innerRadius, "outer": outerRadius}
+                        ])
+            .showLegend(false)
+            .title("0 hours")
+            .titleOffset(10);
 
-    var legend = d3.select("#chart")
-        .append("div")
-        .attr("class", "legend")
-        .selectAll(".legend__item")
-        .data(data.slice(0, data.length - 1))
-        .enter()
-        .append("div")
-        .attr("class", "legend__item");
+            pieChart.tooltip.enabled(true)
+                .hideDelay(0)
+                .headerEnabled(false)
+                .contentGenerator(d => {
+                    if (d === null) {
+                        return '';
+                    }
+                    d3.select(".nvtooltip").classed("mdl-tooltip", true);
+                    return d.data.y + ' hours';
+                });
 
-    legend.append("div")
-        .attr("class", "legend__mark pull-left")
-        .style("background-color", function(d) { return color(d.key); });
+        d3.select(".pie-chart__container")
+            .append("div")
+            .append("svg")
+            .datum(data)
+            .transition().duration(1200)
+            .call(pieChart);
 
-    legend.append("div")
-        .attr("class", "legend__text")
-        .text(function(d) { return d.key; });
+        var h = 0, i = 0;
+        var timer = setInterval(animatePie, 70, data);
 
-    return chart1;
-});
+        function animatePie(data) {
+            if (i < data.length - 1) {
+                if (data[i].y < data[i].end) {
+                    data[i].y++;
+                    data[data.length - 1].y--;
+                    pieChart.title((h + 1) + " hours");
+                    h++;
+                } else {
+                    i++;
+                }
+            } else {
+                data.splice(data.length - 1, 1);
+                clearInterval(timer);
+                return;
+            }
+            pieChart.update();
+        }
 
+        d3.select(".nv-pie .nv-pie")
+            .append("image")
+            .attr("width", "30")
+            .attr("height", "30")
+            .attr("xlink:href", "images/watch_white.svg")
+            .attr("transform", "translate(-15,-35)");
 
+        var color = d3.scale.ordinal().range(colors);
+
+        var legend = d3.select(".pie-chart__container")
+            .append("div")
+            .attr("class", "legend")
+            .selectAll(".legend__item")
+            .data(data.slice(0, data.length - 1))
+            .enter()
+            .append("div")
+            .attr("class", "legend__item");
+
+        legend.append("div")
+            .attr("class", "legend__mark pull-left")
+            .style("background-color", d => {
+                return color(d.key);
+            });
+
+        legend.append("div")
+            .attr("class", "legend__text")
+            .text(d => {
+                return d.key;
+            });
+
+        return pieChart;
+    });
+}
 
