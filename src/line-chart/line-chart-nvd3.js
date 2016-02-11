b40d0eced8cf3842b7db704a63adb2f4aeb8fbfe
alexbananabob <alexbananabob@gmai.com>
2016-02-11 16:39:43 +0300

    line-chart after review, refactoring code, change structure code

--- src/line-chart/line-chart-nvd3.js
+++ src/line-chart/line-chart-nvd3.js
@@ -1,64 +1,27 @@
 {
-    let graphs = [
-        {
-            values: [{x: 0, y: 0}],
-            key: 'Awesome',
-            color: 'rgb(80, 150, 215)'
-        },
-        {
-            values: [{x: 0, y: 0}],
-            key: 'Good',
-            color: 'rgb(0, 188, 212)',
-            fillOpacity: 0.00001,
-            area: true
-        },
-        {
-            values: [{x: 0, y: 0}],
-            key: 'Fail',
-            color: 'rgb(255, 82, 82)'
-        }
-    ];
-
-    class GraphBackground {
-        constructor(columns, color, margin) {
+    class LineChart {
+        constructor(columns, color, margin, data, nv) {
             this.columns = columns;
             this.color = color;
             this.margin = margin;
+            this.data = data;
+            this.nv = nv;
+            this.maxX = 130;
+            this.drawStep = 6; // It shows how many points will be drawn in one step
+            this.durationResizeAnimation = 500;
         }
 
-        addSvgContainer() {
+        _addSvgContainer() {
             this.svg = d3.select('.line-chart__container')
                 .append('div')
                 .append('svg');
         }
 
-        getSvgSizes() {
+        _getSvgSizes() {
             let svgWidth = getComputedStyle(this.svg[0][0]).width,
                 svgHeight = getComputedStyle(this.svg[0][0]).height;
             this.svgWidth = svgWidth.slice(0, svgWidth.length - 2);
             this.svgHeight = svgHeight.slice(0, svgHeight.length - 2) - this.margin;
-        }
-
-        buildBackground() {
-            this.addSvgContainer();
-            this.getSvgSizes();
-
-            let bars = [];
-            for (let i = 0; i < this.columns; i++) {
-                bars.push(this.svgHeight);
-            }
-
-            this.barsLayout = this.svg.append('g')
-                .attr('class', 'bars')
-                .attr('transform', 'translate(' + this.margin + ', 0)')
-                .selectAll('rect')
-                .data(bars)
-                .enter()
-                .append('rect');
-
-            this._addAxisLabels();
-
-            this.resize();
         }
 
         _addAxisLabels() {
@@ -77,7 +40,29 @@
                 .text('TIME')
         }
 
-        resize() {
+        _buildBackground() {
+            this._addSvgContainer();
+            this._getSvgSizes();
+
+            let bars = [];
+            for (let i = 0; i < this.columns; i++) {
+                bars.push(this.svgHeight);
+            }
+
+            this.barsLayout = this.svg.append('g')
+                .attr('class', 'bars')
+                .attr('transform', 'translate(' + this.margin + ', 0)')
+                .selectAll('rect')
+                .data(bars)
+                .enter()
+                .append('rect');
+
+            this._addAxisLabels();
+
+            this._setBackgroundSizes();
+        }
+
+        _setBackgroundSizes() {
             let availableBarWidth = (this.svgWidth - 2 * this.margin) / this.columns,
                 barWidth = availableBarWidth / 2;
             this.barsLayout
@@ -96,35 +81,28 @@
                 .attr('x', this.svgWidth - this.margin - 30)
                 .attr('y', this.svgHeight - (this.svgHeight) / 4 + this.margin + 14)
         }
-    }
-
-    class LineGraph extends GraphBackground {
-        constructor(columns, color, margin, graphs) {
-            super(columns, color, margin);
-            this.graphs = graphs;
-            this.max_X = 130;
-            this.drawStep = 6; // It shows how many points will be drawn in one step
-            this.durationResizeAnimation = 500;
-        }
 
         drawChart () {
-            this.buildBackground();
-            this.buildLegend();
-            this.tuneGraph();
+            this._buildBackground();
+            this._buildLegend();
+            this._buildNvGraph();
+            this._animateGraphs();
+        }
+
+        _buildNvGraph () {
+            this._tuneNvGraph();
 
             nv.addGraph(() => {
-                this.svg.datum(this.graphs)
+                this.svg.datum(this.data)
                     .transition().duration(0)
                     .call(this.lineChart);
-                nv.utils.windowResize(this.graphResize.bind(this));
+                nv.utils.windowResize(this.resizeBackground.bind(this));
                 nv.utils.windowResize(this.lineChart.update);
                 return this.lineChart;
             });
-
-            this.animateGraphs();
         }
 
-        tuneGraph () {
+        _tuneNvGraph () {
             this.lineChart = nv.models.lineChart()
                 .margin({top: this.margin, right: this.margin, bottom: 0, left: this.margin})
                 .useInteractiveGuideline(true)
@@ -149,12 +127,12 @@
                 .tickFormat(d3.format('c'));
         }
 
-        buildLegend () {
+        _buildLegend () {
             let legend = d3.select(".line-chart__container")
                 .append("div")
                 .attr("class", "legend")
                 .selectAll(".legend__item")
-                .data(this.graphs)
+                .data(this.data)
                 .enter()
                 .append("div")
                 .attr("class", "legend__item");
@@ -172,56 +150,56 @@
                 });
         }
 
-        graphResize () {
-            this.getSvgSizes();
-            this.resize();
+        resizeBackground () {
+            this._getSvgSizes();
+            this._setBackgroundSizes();
         }
 
-        animateGraphs () {
+        _animateGraphs () {
             let i = 1;
             this.timer = setInterval(() => {
-                LineGraph.calcAllGraphs(i);
-                this.drawNextStep(i);
+                this._calcAllGraphs(i);
+                this._drawNextStep(i);
                 i++;
-                this.checkEndOfAnimation(i);
+                this._checkEndOfAnimation(i);
 
             }, 15)
         }
 
-        drawNextStep (i) {
-            if (i % this.drawStep == 0 || i == this.max_X) {
+        _drawNextStep (i) {
+            if (i % this.drawStep == 0 || i == this.maxX) {
                 this.lineChart.update();
             }
         }
 
-        checkEndOfAnimation (i) {
-            if (i == this.max_X + 1) {
+        _checkEndOfAnimation (i) {
+            if (i == this.maxX + 1) {
                 this.lineChart.duration(this.durationResizeAnimation);
-                this.graphs[1].fillOpacity = 0.11;
+                this.data[1].fillOpacity = 0.11;
                 this.lineChart.update();
                 clearInterval(this.timer);
             }
         }
 
-        static calcAllGraphs (i) {
-            LineGraph.calcFirstGraph(i);
-            LineGraph.calcSecondGraph(i);
-            LineGraph.calcThirdGraph(i);
+        _calcAllGraphs (i) {
+            this._calcFirstGraph(i);
+            this._calcSecondGraph(i);
+            this._calcThirdGraph(i);
         }
 
-        static calcFirstGraph (i) {
-            const interval_1 = 28,
-                interval_2 = 71,
-                interval_3 = 110;
-            let graphAwesome = graphs[0].values;
+        _calcFirstGraph (i) {
+            const INTERVAL_1 = 28,
+                INTERVAL_2 = 71,
+                INTERVAL_3 = 110;
+            let graphAwesome = this.data[0].values;
 
-            if (i < interval_1) {
+            if (i < INTERVAL_1) {
                 graphAwesome.push({x: i / 10, y: (.0343 * i * i - .67 * i) / 14});
             } else {
-                if (i < interval_2) {
+                if (i < INTERVAL_2) {
                     graphAwesome.push({x: i / 10, y: -(i - 71) * (i - 71) / 1026 + 2.378});
                 } else {
-                    if (i < interval_3) {
+                    if (i < INTERVAL_3) {
                         graphAwesome.push({x: i / 10, y: -4 / (i - 43) + 2.53});
                     } else {
                         graphAwesome.push({x: i / 10, y: ((i - 114) * (i - 114) * (i - 114)) / 13000 + 2.476});
@@ -230,15 +208,15 @@
             }
         }
 
-        static calcSecondGraph (i) {
-            const interval_1 = 30,
-                interval_2 = 82;
-            let graphGood = graphs[1].values;
+        _calcSecondGraph (i) {
+            const INTERVAL_1 = 30,
+                INTERVAL_2 = 82;
+            let graphGood = this.data[1].values;
 
-            if (i < interval_1) {
+            if (i < INTERVAL_1) {
                 graphGood.push({x: i / 10, y: (.03255 * i * i - .96 * i) / 16});
             } else {
-                if (i < interval_2) {
+                if (i < INTERVAL_2) {
                     graphGood.push({x: i / 10, y: (-.01055 * (i - 80.3) * (i - 80.3) + 27) / 15});
                 } else {
                     graphGood.push({x: i / 10, y: (((i / 2) - 45) * ((i / 2) - 45) * ((i / 2) - 45)) / 15000 + 1.805});
@@ -246,24 +224,43 @@
             }
         }
 
-        static calcThirdGraph (i) {
-            const interval_1 = 31,
-                interval_2 = 103;
-            let graphFail = graphs[2].values;
+        _calcThirdGraph (i) {
+            const INTERVAL_1 = 31,
+                INTERVAL_2 = 103;
+            let graphFail = this.data[2].values;
 
-            if (i < interval_1) {
+            if (i < INTERVAL_1) {
                 graphFail.push({x: i / 10, y: (.02255 * i * i - .91 * i) / 13});
             } else {
-                if (i < interval_2) {
+                if (i < INTERVAL_2) {
                     graphFail.push({x: i / 10, y: .82 * Math.sin((i - 45) / 21)});
                 } else {
                     graphFail.push({x: i / 10, y: -(i - 130) * (i - 130) * (i - 130) / 64000});
                 }
             }
         }
-
     }
 
-    let myChart = new LineGraph(7, '#4a4a4a', 20, graphs);
-    myChart.drawChart();
+    let data = [
+        {
+            values: [{x: 0, y: 0}],
+            key: 'Awesome',
+            color: 'rgb(80, 150, 215)'
+        },
+        {
+            values: [{x: 0, y: 0}],
+            key: 'Good',
+            color: 'rgb(0, 188, 212)',
+            fillOpacity: 0.00001,
+            area: true
+        },
+        {
+            values: [{x: 0, y: 0}],
+            key: 'Fail',
+            color: 'rgb(255, 82, 82)'
+        }
+    ];
+
+    let lineChart = new LineChart(7, '#4a4a4a', 20, data, nv);
+    lineChart.drawChart();
 }
