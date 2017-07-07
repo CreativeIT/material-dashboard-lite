5d9e4c1ee6cf78ca00c2fc366c8bc4f72ea45d27
DESKTOP-5NSJLHS\chery <cheryboom100@gmail.com>
2017-07-07 19:21:44 +0300

    Nice scrollbar on nav added

---
+++ src/scroll/scroll.js
@@ -0,0 +1,45 @@
+'use strict';
+
+{
+    document.addEventListener("DOMContentLoaded", () => {
+        let wrapper = document.getElementById('scroll__wrapper'),
+            scroller = document.getElementById('scroller'),
+            container = document.getElementById('scroll__container'),
+            scrollbar = document.getElementById('scroller__bar'),
+            scrollBarHeight;
+
+        let scrollWidth = scroller.offsetWidth - scroller.clientWidth;
+        scroller.style.width = 'calc(100% + ' + scrollWidth +'px)';
+
+        console.log('scrollWidth', scroller.style.width);
+
+        scroller.addEventListener('scroll', () => {
+            if (scrollBarHeight > 8) {
+                scrollbar.style.top = 2 + scroller.scrollTop + 'px';
+            } else {                                            //need to find coefficient
+                scrollbar.style.top = 2 + scroller.scrollTop*((scroller.offsetHeight - 12)/(container.offsetHeight - scroller.offsetHeight)) + 'px'; //It's Math, yeah
+            }
+
+            console.log(container.offsetHeight, scroller.offsetHeight, scroller.scrollTop);
+        });
+
+        function calcBarHeight () {
+            console.log(container.offsetHeight, scroller.offsetHeight);
+            if (container.offsetHeight > scroller.offsetHeight) {
+                scrollBarHeight = scroller.offsetHeight - (container.offsetHeight - scroller.offsetHeight) - 4;
+                if (scrollBarHeight > 8) {
+                    scrollbar.style.height = scrollBarHeight + 'px';
+                } else {
+                    scrollbar.style.height = '8px';
+                }
+            } else {
+                scrollbar.style.height = 0;
+            }
+        }
+
+        wrapper.addEventListener('click', calcBarHeight);
+        window.addEventListener('resize', calcBarHeight);
+
+        calcBarHeight();
+    });
+}
